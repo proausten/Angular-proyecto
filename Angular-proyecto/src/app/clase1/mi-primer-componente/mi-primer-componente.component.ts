5a25962897edf82bb3e8204f68d399f4665b78d2
proausten <proausten@hotmail.com>
2022-11-24 14:11:23 -0300

    Comandos Diego Sosa

---
+++ Angular-proyecto/src/app/clase1/mi-primer-componente/mi-primer-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-primer-componente',
+  templateUrl: './mi-primer-componente.component.html',
+  styleUrls: ['./mi-primer-componente.component.scss']
+})
+export class MiPrimerComponenteComponent {
+
+}
